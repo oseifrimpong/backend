22f0d5b1f6cf8cccaddccd2994be70116aa4d389
obedDecath <obedfrimpong.osei@decathlon.com>
2023-11-06 09:06:07 +0800

    update

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	// Init context
 	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(-1)
@@ -15,21 +14,6 @@ func main() {
 }
 
 func run() error {
-
-	// g := gin.Default()
-
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	// Or instantiate a client with a custom bin and args options
-	// 	llm, err := ollama.New(ollama.WithModel("llama2"))
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	ctx := context.Background()
-
-	// 	c.String(http.StatusOK, completion)
-	// })
-
 	server.InitializeServer()
-
 	return nil
 }
